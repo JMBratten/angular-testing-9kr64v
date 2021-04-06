e9298657ced5baa1f5cf73fd4bd79bde82813aed
JMBratten <jbratten@agvance.net>
2021-04-06 17:01:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-testing-9kr64v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-testing-9kr64v)
---
+++ app/app.component.spec.ts
@@ -0,0 +1,74 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement }  from '@angular/core';
+import { MockComponent } from 'ng2-mock-component';
+import dedent from "dedent";
+
+import { AppComponent } from './app.component';
+
+const errorMessage = 'This is the error!';
+function throwIt() {
+  throw new Error(errorMessage);
+}
+
+describe('Integration::AppComponent', () => {
+  it('throws', () => {
+    console.log('throw');
+    try {
+      throwIt();
+    }
+    catch(error) {
+      console.log('error', error);
+      expect(error).toEqual(new Error(errorMessage));
+    }
+  });
+
+  it('should display some text', () => {
+    // given
+    const { comp, el, fixture } = setup();
+
+    // when
+    fixture.detectChanges();
+
+    // then
+    expect(el.textContent).toContain(dedent`
+    Start editing to see some magic happen :)
+    `);
+  });
+
+  it('should contain hello component', () => {
+    // given
+    const { comp, el, fixture } = setup();
+
+    // when
+    fixture.detectChanges();
+
+    // then
+    expect(el.textContent).toContain('hello');
+  });
+});
+
+//
+
+function setup () {
+  
+  TestBed.configureTestingModule({
+    declarations: [
+      AppComponent,
+      MockComponent({
+        selector: 'hello', 
+        template: 'hello',
+        inputs: ['name']
+      })
+    ],
+  });
+
+  const fixture = TestBed.createComponent(AppComponent);
+
+  const comp = fixture.componentInstance;
+
+  const de = fixture.debugElement;
+  const el = de.nativeElement;
+
+  return {comp, fixture, de, el}
+}
